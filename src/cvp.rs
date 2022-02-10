41c63686aac6e749664aa9cb10cb43f01ac1ffef
Fred Hsu <fredlhsu@gmail.com>
2022-02-09 19:13:05 -0800

    first

---
+++ src/cvp.rs
@@ -0,0 +1,93 @@
+use reqwest::header::*;
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+pub struct Host {
+    hostname: String,
+    port: u32,
+    username: String,
+    password: String,
+    token: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TokenResponse {
+    token: String,
+}
+
+impl Host {
+    pub fn new(hostname: &str, port: u32, username: &str, password: &str) -> Self {
+        Host {
+            hostname: hostname.to_string(),
+            port,
+            username: username.to_string(),
+            password: password.to_string(),
+            token: None,
+        }
+    }
+
+    pub fn get_token_from_file(&mut self, filename: String) -> Result<(), std::io::Error> {
+        let t = fs::read_to_string(filename)?;
+        let token = t.trim().to_string();
+        self.token = Some(token);
+        Ok(())
+    }
+
+    // get_token is only useful for on prem CVP
+    pub async fn get_token_from_auth(&mut self) -> Result<(), reqwest::Error> {
+        let path = "/cvpservice/login/authenticate.do";
+        let url = format!("https://{}:{}{}", self.hostname, self.port, path);
+        let client = reqwest::Client::new();
+        let response = client
+            .post(url)
+            .basic_auth(&self.username, Some(&self.password))
+            .send()
+            .await?
+            .json::<TokenResponse>()
+            .await?;
+        self.token = Some(response.token);
+        Ok(())
+    }
+
+    async fn get(&self, path: &str) -> Result<String, reqwest::Error> {
+        if let Some(token) = &self.token {
+            let url = format!("https://{}{}", self.hostname, path);
+            let client = reqwest::Client::new();
+            let response = client
+                .get(url)
+                .header(ACCEPT, "application/json")
+                .bearer_auth(token.to_string())
+                .send()
+                .await?
+                .text()
+                .await?;
+            Ok(response)
+        } else {
+            // TODO: use an error to indicate no token
+            Ok("".to_string())
+        }
+    }
+    pub async fn get_all_devices(&self) -> Result<String, reqwest::Error> {
+        let path = "/api/resources/inventory/v1/Device/all";
+        self.get(path).await
+    }
+    pub async fn get_device(&self, device_id: &str) -> Result<String, reqwest::Error> {
+        let path = format!("/api/resources/inventory/v1/Device?key.deviceId={}", device_id);
+        self.get(&path).await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_get_token_from_file() {
+        let mut cv = Host::new("foo", 443, "user", "pass");
+        cv.get_token_from_file("token.txt".to_string());
+        if let Some(token) = cv.token {
+            assert!(token.starts_with("ey"));
+        } else {
+            panic!("did not read file");
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,263 @@
+use reqwest::header::*;
+use serde::{Deserialize, Serialize};
+//use serde_json::Result;
+use std::net::TcpStream;
+use tungstenite::stream::MaybeTlsStream;
+use tungstenite::{connect, Message};
+use url::Url;
+mod cvp;
+
+#[derive(Deserialize, Debug)]
+struct AppsConnectionsOpenResponse {
+    ok: bool,
+    url: Option<String>,
+    error: Option<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct ChannelsResponse {
+    ok: bool,
+    channels: Vec<Channel>,
+}
+
+#[derive(Deserialize, Debug)]
+struct Channel {
+    id: String,
+    name: String,
+    is_channel: bool,
+    created: u32,
+    creator: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type")]
+enum SocketEvent {
+    #[serde(rename = "events_api")]
+    EventsApi {
+        payload: EventCallback,
+        envelope_id: String,
+        accepts_response_payload: bool,
+    },
+    #[serde(rename = "slash_commands")]
+    SlashCommands {
+        payload: SlashCommand,
+        envelope_id: String,
+        accepts_response_payload: bool,
+    },
+}
+
+#[derive(Deserialize, Debug)]
+struct SlashCommand {
+    token: String,
+    team_id: String,
+    team_domain: String,
+    channel_id: String,
+    channel_name: String,
+    user_id: String,
+    user_name: String,
+    command: String,
+    text: String,
+    api_app_id: String,
+    is_enterprise_install: String,
+    response_url: String,
+    trigger_id: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Response {
+    envelope_id: String,
+    payload: BlockPayload,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct BlockPayload {
+    blocks: Vec<Block>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Block {
+    #[serde(rename = "type")]
+    block_type: String,
+    text: TextBlock,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TextBlock {
+    #[serde(rename = "type")]
+    text_type: String,
+    text: String,
+}
+
+// TODO: create enum for event payloads and event_types
+#[derive(Deserialize, Debug)]
+#[serde(tag = "type")]
+enum EventPayload {
+    #[serde(rename = "event_callback")]
+    EventCallback(EventCallback),
+}
+
+#[derive(Deserialize, Debug)]
+struct EventCallback {
+    token: String,
+    team_id: String,
+    event: AppMention,
+    event_id: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Hello {
+    num_connections: u32,
+    debug_info: DebugInfo,
+    connection_info: ConnectionInfo,
+}
+#[derive(Deserialize, Debug)]
+struct DebugInfo {
+    host: String,
+    build_number: u32,
+    approximate_connection_time: u32,
+}
+#[derive(Deserialize, Debug)]
+struct ConnectionInfo {
+    app_id: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct AppMention {
+    #[serde(rename = "type")]
+    event_type: String,
+    user: String,
+    text: String,
+    ts: String,
+    channel: String,
+    event_ts: String,
+}
+
+#[derive(Deserialize, Debug)]
+enum EventType {
+    #[serde(rename = "slash_command")]
+    SlashCommand,
+    #[serde(rename = "events_api")]
+    EventsAPI,
+    #[serde(rename = "hello")]
+    Hello,
+    #[serde(rename = "event_callback")]
+    EventCallback,
+}
+
+async fn get_channels(base_url: String, oauth_token: &str) -> Result<(), reqwest::Error> {
+    let client = reqwest::Client::new();
+    let conversations_url = format!("{}{}", base_url, "conversations.list");
+
+    let response = client
+        .get(conversations_url)
+        .header(AUTHORIZATION, "Bearer ".to_owned() + oauth_token)
+        .send()
+        .await?
+        .json::<ChannelsResponse>()
+        .await?;
+    println!("body = {:?}", response);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let mut cv = cvp::Host::new(
+        "www.cv-staging.corp.arista.io",
+        443,
+        "fredlhsu@arista.com",
+        "arista",
+    );
+    cv.get_token_from_file("token.txt".to_string());
+    let inventory = cv.get_all_devices().await?;
+    println!("{}", inventory);
+    let device = cv.get_device("F799ECF9B7DA78B0BC849B972D16E373").await?;
+    println!("device: {:?}", device);
+
+    let oauth_token = "xoxb-2203743897442-3066349622661-dNflZ6ntVqcKw5gf6YVx9AQP";
+    let socket_token = "xapp-1-A031UGBQ6RL-3098313919440-9c157c4d8cb6ac1200567ee7ee7d01d8e8f5a8e8e2a53f03aea94a17e5c0ccb2";
+
+    let base_url = "https://slack.com/api/".to_owned();
+    let client = reqwest::Client::new();
+    let connection_response = client
+        .post(base_url + "apps.connections.open")
+        .header(AUTHORIZATION, "Bearer ".to_owned() + socket_token)
+        .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
+        .send()
+        .await?
+        .json::<AppsConnectionsOpenResponse>()
+        .await?;
+
+    let wss_url = connection_response.url.unwrap();
+    println!("wss_url = {:?}", wss_url);
+    let (mut socket, response) = connect(Url::parse(&wss_url).unwrap()).expect("Can't connect");
+    let msg = socket.read_message().expect("Error reading message");
+    //let hello: Hello = serde_json::from_str(&msg).unwrap();
+    println!("recevied hello: {:?}", msg);
+    loop {
+        let msg = socket.read_message().expect("Error reading message");
+        if let tungstenite::Message::Text(msg) = msg {
+            println!("Received message: {}", msg);
+            let socket_event: SocketEvent = serde_json::from_str(&msg).unwrap();
+            println!("Received: {:?}", socket_event);
+            match socket_event {
+                SocketEvent::EventsApi {
+                    payload,
+                    envelope_id,
+                    accepts_response_payload,
+                } => {
+                    println!("{:?}", payload);
+                    if payload.event.text.ends_with("quit") {
+                        break;
+                    }
+                }
+                SocketEvent::SlashCommands {
+                    payload,
+                    envelope_id,
+                    accepts_response_payload,
+                } => {
+                    println!("{} is : {:?}", payload.command, payload.text);
+                    handle_slash_command(&mut socket, payload, envelope_id);
+                }
+                _ => {}
+            }
+        }
+        // send ack back to slack with envelope_id
+    }
+    Ok(())
+}
+
+fn handle_slash_command(
+    socket: &mut tungstenite::WebSocket<MaybeTlsStream<TcpStream>>,
+    payload: SlashCommand,
+    envelope_id: String,
+) {
+    let block_type = "section";
+    let text_type = "mrkdwn";
+    let block1 = Block {
+        block_type: block_type.to_string(),
+        text: TextBlock {
+            text_type: text_type.to_string(),
+            text: "#switch 1".to_owned(),
+        },
+    };
+    let block2 = Block {
+        block_type: block_type.to_string(),
+        text: TextBlock {
+            text_type: text_type.to_string(),
+            text: "port 1".to_owned(),
+        },
+    };
+    let blocks = vec![block1, block2];
+    let payload = BlockPayload { blocks };
+
+    // send block back as resposne
+    let response = Response {
+        envelope_id,
+        payload,
+    };
+    let response_json = serde_json::to_string(&response).unwrap();
+    println!("send message {}", &response_json);
+    socket
+        .write_message(Message::Text(response_json.into()))
+        .unwrap();
+}
