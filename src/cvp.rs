71469a5c2190eb35f031b18e9dad1080c01a123a
Fred Hsu <fredlhsu@gmail.com>
2022-02-15 17:34:26 -0800

    cloudvision queries working

--- src/cvp.rs
+++ src/cvp.rs
@@ -19,6 +19,72 @@ pub struct CookieResponse {
     #[serde(rename="Value")]
     value: String,
 }
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PartialEqFilter {
+    pub partial_eq_filter: Vec<Tag>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Tag {
+    pub key: TagKey,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct TagKey {
+    pub workspace_id: String,
+    pub element_type: Option<String>,
+    //TODO make elementtype enum
+    pub label: Option<String>,
+    pub value: Option<String>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all="SCREAMING_SNAKE_CASE")]
+pub enum ElementType {
+    ElementTypeUnspecified,
+    ElementTypeDevice,
+    ElementTypeInterface,
+} 
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct InterfaceQueryResponse {
+    pub value: Vec<InterfaceResponse>,
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct InterfaceResponse {
+    pub value: Interface,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Interface {
+    pub key:InterfaceKey, 
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct InterfaceKey {
+    pub workspace_id:String, 
+    pub element_type:String, 
+    pub label:String, 
+    pub value:String, 
+    pub device_id:String, 
+    pub interface_id:String, 
+}
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Device {
+    pub key:DeviceKey, 
+    pub software_version:String, 
+    pub model_name:String, 
+    pub hardware_revision:String, 
+    pub fqdn:String, 
+    pub hostname:String, 
+    pub domain_name:String, 
+    pub system_mac_address:String, 
+    pub boot_time:String, 
+    pub streaming_status:String, 
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct DeviceKey {
+    #[serde(rename="deviceId")]
+    pub device_id: String,
+}
+
 
 impl Host {
     pub fn new(hostname: &str, port: u32, username: &str, password: &str) -> Self {
@@ -78,7 +144,7 @@ impl Host {
             Ok("".to_string())
         }
     }
-    async fn post(&self, path: &str) -> Result<String, reqwest::Error> {
+    async fn post(&self, path: &str, body: String) -> Result<String, reqwest::Error> {
         if let Some(token) = &self.token {
             let url = format!("https://{}{}", self.hostname, path);
         let client = reqwest::Client::builder()
@@ -88,6 +154,7 @@ impl Host {
                 .post(url)
                 .header(ACCEPT, "application/json")
                 .bearer_auth(token.to_string())
+                .body(body)
                 .send()
                 .await?
                 .text()
@@ -100,7 +167,25 @@ impl Host {
     }
     pub async fn get_tags(&self) -> Result<String, reqwest::Error> {
         let path = "/api/resources/tag/v2/Tag/all";
-        self.post(path).await
+        // TODO: replace this with the url above when cvaas is fixed
+        // let path = "/api/v3/services/arista.tag.v2.Tag/GetAll";
+        let workspace_key = TagKey { workspace_id: "".to_string(), element_type: None, label: None, value: None };
+        let filter = Tag {
+            key: workspace_key,
+        };
+        let data = PartialEqFilter {
+            partial_eq_filter: vec![filter],
+        };
+        let json_data = serde_json::to_string(&data).unwrap();
+        self.post(path, json_data).await
+    }
+    pub async fn get_tag_assignment(&self, partial_eq_filter: PartialEqFilter) -> Result<String, reqwest::Error> {
+        // let path = "/api/resources/tag/v2/TagAssignment/all";
+        // TODO: replace this with the url above when cvaas is fixed
+        let path = "/api/v3/services/arista.tag.v2.TagAssignmentService/GetAll";
+        let json_data = serde_json::to_string(&partial_eq_filter).unwrap();
+        println!("Filter is : {}", json_data);
+        self.post(path, json_data).await
     }
 
     pub async fn get_all_devices(&self) -> Result<String, reqwest::Error> {
@@ -122,7 +207,7 @@ mod tests {
     #[test]
     fn test_get_token_from_file() {
         let mut cv = Host::new("foo", 443, "user", "pass");
-        cv.get_token_from_file("token.txt".to_string());
+        cv.get_token_from_file("token.txt".to_string()).unwrap();
         if let Some(token) = cv.token {
             assert!(token.starts_with("ey"));
         } else {
--- src/main.rs
+++ src/main.rs
@@ -104,30 +104,69 @@ async fn get_channels(base_url: String, oauth_token: &str) -> Result<(), reqwest
     Ok(())
 }
 
-#[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    // let mut cv = cvp::Host::new(
-    //     "www.cv-staging.corp.arista.io",
-    //     443,
-    //     "fredlhsu@arista.com",
-    //     "arista",
-    // );
-    let mut cv = cvp::Host::new(
-        "10.90.226.175",
-        443,
-        "cvpadmin",
-        "arista123!",
-    );
-    // cv.get_token_from_file("tokens/token.txt".to_string())
-    //     .unwrap();
-    cv.get_token_from_auth().await.unwrap();
+async fn get_tags(cv: &cvp::Host) -> Result<(), reqwest::Error>{
+    let tags = cv.get_tags().await?;
+    println!("Tags: {}", tags);
+    Ok(())
+}
+async fn get_tag_assignment(cv: &cvp::Host, label: String, value: String) -> Result<String, reqwest::Error>{
+    /*
+    workspace_id: "",
+    element_type: ELEMENT_TYPE_INTERFACE,
+    label: "wall_jack",
+    value: "sjc1-401-019-1",
+    */
+    /* Jack numbering
+    Jack number format:  BBf-rrr-ppp-j
+where   BB        = two letter building code
+            f           = floor number
+            rr        = room number
+            pp      = plate number
+            j           = number of jack on that plate
+            */
+        let workspace_key = cvp::TagKey { workspace_id: "".to_string(), element_type: Some("ELEMENT_TYPE_INTERFACE".to_string()), label: Some(label.to_string()), value: Some(value.to_string()) };
+        let filter = cvp::Tag {
+            key: workspace_key,
+        };
+        let data = cvp::PartialEqFilter {
+            partial_eq_filter: vec![filter],
+        };
+    cv.get_tag_assignment(data).await
+}
+
+async fn get_inventory(cv: &cvp::Host)-> Result<(), reqwest::Error> {
     let inventory = cv.get_all_devices().await?;
     println!("Getting Inventory");
     println!("{}", inventory);
-    // let device = cv.get_device("F799ECF9B7DA78B0BC849B972D16E373").await?;
-    // println!("device: {:?}", device);
-    let tags = cv.get_tags().await?;
-    println!("Tags: {}", tags);
+    Ok(())
+}
+
+async fn get_device(cv: &cvp::Host)-> Result<(), reqwest::Error> {
+    let device = cv.get_device("JPE12233288").await?;
+    println!("device: {:?}", device);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let mut cv = cvp::Host::new(
+        "www.cv-staging.corp.arista.io",
+        443,
+        "fredlhsu@arista.com",
+        "arista",
+    );
+    // let mut cv = cvp::Host::new(
+    //     "10.90.226.175",
+    //     443,
+    //     "cvpadmin",
+    //     "arista123!",
+    // );
+    cv.get_token_from_file("tokens/token.txt".to_string())
+        .unwrap();
+    // cv.get_token_from_auth().await.unwrap();
+    // get_tag_assignment(&cv, "wall_jack".to_string(), "sjc1-401-019-1".to_string()).await.unwrap();
+    // get_device(&cv).await.unwrap();
+
 
     let slack_token = slack::Client::get_token_from_file("tokens/slack.token").unwrap();
     let mut slack = slack::Client::new(slack_token);
@@ -137,18 +176,18 @@ async fn main() -> Result<(), reqwest::Error> {
     loop {
         let msg = slack.receive_message().await.unwrap();
         match msg {
-            Message::Text(t) => handle_text(&t, &mut slack).await,
-            Message::Binary(b) => println!("binary"),
-            Message::Ping(p) => println!("{:?}", p),
-            Message::Pong(p) => println!("{:?}", p),
+            Message::Text(t) => handle_text(&cv, &t, &mut slack).await,
+            Message::Binary(_) => println!("binary"),
+            Message::Ping(p) => {},
+            Message::Pong(p) => {},
             Message::Close(_) => break,
         }
     }
     Ok(())
 }
 
-async fn handle_text(t: &str, slack: &mut slack::Client) {
-    println!("*** Incoming text *** \n {:?}", t);
+async fn handle_text(cv: &cvp::Host, t: &str, slack: &mut slack::Client) {
+    // println!("*** Incoming text *** \n {:?}", t);
     let socket_event = slack::parse_message(t);
     println!("{:?}", &socket_event);
     match socket_event {
@@ -165,7 +204,7 @@ async fn handle_text(t: &str, slack: &mut slack::Client) {
             accepts_response_payload,
         } => {
             println!("Received slash command: {:?}", payload);
-            handle_slash_command(slack, payload, envelope_id);
+            handle_slash_command(cv, slack, payload, envelope_id).await;
         }
         slack::SocketEvent::Interactive {
             payload,
@@ -176,7 +215,6 @@ async fn handle_text(t: &str, slack: &mut slack::Client) {
             handle_interactive(slack, payload).await;
             println!("response sent");
         }
-        _ => {}
     }
 }
 
@@ -214,51 +252,36 @@ async fn handle_interactive(
 // sends notification to channel with @ of admins
 // /portdown walljack
 // /portup walljack
-fn handle_slash_command(
+async fn handle_slash_command(
+    cv: &cvp::Host,
     slack: &mut slack::Client,
     payload: slack::SlashCommand,
     envelope_id: String,
 ) {
     let command = &payload.get_command();
+    println!("Command is: {}", &command);
     match command.as_str() {
-        "portcheck" => portcheck(&payload.text, &envelope_id, slack),
+        "portcheck" => portcheck(cv, &payload.text, &envelope_id, slack).await,
         "portassign" => port_assign(&payload.text, &envelope_id, slack),
         "portdown" => println!("Shutting down port {}", &payload.text),
         "portup" => println!("Bringing port {} up", &payload.text),
         _ => println!("Unknown command"),
     }
-    // let first = format!("Getting status for port {}", payload.text);
-    // let placeholder = TextBlock::new_plain("placeholder".to_string());
-    // let option1 = OptionObject::new(
-    //     TextBlock::new_plain("this is plain".to_string()),
-    //     "value-0".to_string(),
-    // );
-    // let accessory = StaticSelect::new(placeholder, "action123".to_string(), vec![option1]);
-    // let mut block1 = Block::new_section(TextBlock::new_mrkdwn(first));
-    // block1.add_accessory(accessory);
-    // let block2 = Block::new_section(TextBlock::new_mrkdwn("This is another test".to_owned()));
-    // let blocks = vec![block1, block2];
-    // let payload = BlockPayload::new(blocks);
-
-    // send block back as resposne
-    // let response = Response {
-    //     envelope_id,
-    //     payload,
-    // };
-    // let response_json = serde_json::to_string(&response).unwrap();
-    // slack.send_message(&response_json);
-    // println!("slack wrote message: {}", response_json);
 }
 
-fn portcheck(text: &str, envelope_id: &str, slack: &mut slack::Client) {
-    let walljack = text;
-    let switchport = "Eth 1/1";
-    let switch_id = "JPE123";
-    let segment_id = "USER:VLAN100";
+async fn portcheck(cv: &cvp::Host, walljack: &str, envelope_id: &str, slack: &mut slack::Client) {
+    let device_json = get_tag_assignment(&cv, "wall_jack".to_string(), walljack.to_string()).await.unwrap();
+    println!("devicejson: {}", device_json);
+    let device : Vec<cvp::InterfaceResponse> = serde_json::from_str(&device_json).unwrap();
+    let first_device = &device.first().unwrap().value.key;
+    println!("portcheck device: {}", &first_device.device_id);
+    // let resp_text = format!(
+    //     "Wall jack: {} is connected to port {} on switch {} and is in segement ID {}",
+    //     walljack, &first_device.interface_id, &first_device.device_id, segment_id
+    // );
     let resp_text = format!(
-        "Wall jack: {} is connected to port {} on switch {} and is in segement ID {}",
-        walljack, switchport, switch_id, segment_id
-    );
+        "Wall jack: {} is connected to port {} on switch {}",
+        walljack, &first_device.interface_id, &first_device.device_id);
     let block2 = Block::new_section(TextBlock::new_mrkdwn(resp_text));
     let blocks = vec![block2];
     let payload = BlockPayload::new(blocks);
